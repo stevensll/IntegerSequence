47ecaf1bb4b3781d9600893dd8629e3c25282789
Steven <deltaorb637@gmail.com>
2020-12-07 13:42:56 -0500

    Created ArraySequence class.

---
+++ ArraySequence.java
@@ -0,0 +1,15 @@
+public class ArraySequence implements IntegerSequence{
+    public boolean hasNext(){
+        return false;
+    }
+    public int next(){
+        return 0;
+    }
+    public int length(){
+        return 0;
+    } 
+    public void reset(){
+
+    }
+
+}
