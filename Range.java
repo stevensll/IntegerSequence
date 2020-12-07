b747a60d75e3947903f1a051e1d133a7220823f5
Steven <deltaorb637@gmail.com>
2020-12-06 21:33:04 -0500

    Added constructor throw

--- Range.java
+++ Range.java
@@ -1,9 +1,11 @@
 import java.util.NoSuchElementException;
+
 public class Range implements IntegerSequence{
     private int start, end, current;
         //@param start : the starting value (inclusive) which must be <= end.
         //@param end : the ending value which is also inclusive.
     public Range(int start, int end){
+        if (start > end) throw new IllegalArgumentException("start value must be <= end value");
         this.start = start;
         this.end = end;
         current = start;
