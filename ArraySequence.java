302606a99976af624c5efe535e08f993b868bbf5
Steven <deltaorb637@gmail.com>
2020-12-07 14:14:12 -0500

    Wrote and tested methods for ArraySequence class.

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,15 +1,27 @@
+import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
+    private int currentIndex;
+    private int[] data;
+    public ArraySequence(int [] other){
+        data = new int[other.length];
+        for (int i = 0; i < other.length; i++){
+            data[i] = other[i];
+        }
+        currentIndex = 0;
+    }
     public boolean hasNext(){
-        return false;
+        return currentIndex <= data.length-1;
     }
     public int next(){
-        return 0;
+        if (!hasNext()) throw new NoSuchElementException("No next value in sequence.");
+        currentIndex++;
+        return data[currentIndex-1];
     }
     public int length(){
-        return 0;
+        return data.length;
     } 
     public void reset(){
-
+        currentIndex = 0;
     }
 
 }
