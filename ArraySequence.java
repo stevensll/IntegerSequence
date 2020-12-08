326b085d6555372229ff0ae5fc1fc64d88c93525
Steven <deltaorb637@gmail.com>
2020-12-08 15:34:06 -0500

    Added reset to constructor for IntegerSequence inputs.

--- ArraySequence.java
+++ ArraySequence.java
@@ -10,6 +10,7 @@ public class ArraySequence implements IntegerSequence{
         currentIndex = 0;
     }
     public ArraySequence(IntegerSequence otherseq){
+        otherseq.reset();
         data = new int[otherseq.length()];
         for (int i = 0; i < otherseq.length(); i++){
             data[i] = otherseq.next();
