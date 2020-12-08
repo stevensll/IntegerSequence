44860efa23d9434bf53b3d46ed34ed707210331b
Steven <deltaorb637@gmail.com>
2020-12-08 16:04:23 -0500

    Updated tester.

--- StevenTester.java
+++ StevenTester.java
@@ -2,6 +2,7 @@ import java.util.NoSuchElementException;
 
 public class StevenTester {
     public static void main(String args[]){
+        /*
         IntegerSequence a = new Range(10,15);
         IntegerSequence b = new Range(1,15);
         IntegerSequence c = new Range(-30,15);
@@ -12,7 +13,7 @@ public class StevenTester {
             if (i!=15) cAsString += i + ", ";
             else cAsString+=i;
         }
-
+        
         String testOut = "good";
         if (a.length() != 6) throw new Error("a.length() does not match with length 6.");
         if (b.length() != 15) throw new Error("b.length() does not match with length 15.");
@@ -32,7 +33,27 @@ public class StevenTester {
             testOut = "reset() failed on IntegerSequence b";
         }
         System.out.println(testOut);
+        */
+        int[]nums = {1,3,5,0,-1,3,9};
+        int[]nums2 = {93, 94, 0, 21, -3};
+        IntegerSequence a1 = new ArraySequence(nums);
+        IntegerSequence a2 = new ArraySequence(nums2);
+        System.out.println("constructor test: good");
+
+        if(a1.length()!=7) throw new Error("a1.length() does not match with length 7.");
+        if(a2.length()!=5) throw new Error("a1.length() does not match with length 7.");
+
+
+        IntegerSequence r = new Range(-20,50);
+        IntegerSequence boo = new ArraySequence(r);
+        System.out.println(boo.length());
+        System.out.println("ArraySequence(seq):");
+        while(boo.hasNext()){
+            System.out.print(boo.next()+", ");
+          }
+          System.out.println();
     }
+
     public static String sequenceToString(IntegerSequence r){
         String str = "";
         while(r.hasNext()){
